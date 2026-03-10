gao-yan/pacemaker

commit d7cc9b45a7a78e5bf92bf1edb809f87b5ead1128

API: libcrmcommon: New "fencing-watchdog-timeout" cluster property
To replace stonith-watchdog-timeout. "stonith-watchdog-timeout" is still
supported as a fallback for the time being.

Ref T895

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

daemons/fenced/fenced_cib.c
@@ -144,7 +144,7 @@ fencing_topology_init(void)
 
 #define XPATH_WATCHDOG_TIMEOUT "//" PCMK_XE_NVPAIR      \
                                "[@" PCMK_XA_NAME "='"   \
-                                    PCMK_OPT_STONITH_WATCHDOG_TIMEOUT "']"
+                                    PCMK_OPT_FENCING_WATCHDOG_TIMEOUT "']"
 
 static void
 update_stonith_watchdog_timeout_ms(xmlNode *cib)

lib/lrmd/lrmd_client.c
@@ -982,7 +982,7 @@ lrmd__validate_remote_settings(lrmd_t *lrmd, GHashTable *hash)
 
     pcmk__xe_set(data, PCMK__XA_LRMD_ORIGIN, __func__);
 
-    value = g_hash_table_lookup(hash, PCMK_OPT_STONITH_WATCHDOG_TIMEOUT);
+    value = g_hash_table_lookup(hash, PCMK_OPT_FENCING_WATCHDOG_TIMEOUT);
     if ((value) &&
         (stonith__watchdog_fencing_enabled_for_node(native->remote_nodename))) {
        pcmk__xe_set(data, PCMK__XA_LRMD_WATCHDOG, value);

daemons/controld/controld_control.c
@@ -596,7 +596,7 @@ config_query_callback(xmlNode * msg, int call_id, int rc, xmlNode * output, void
      * A validator function in libcrmcommon can't act as such a wrapper, because
      * it doesn't have a stonith API connection or the local node name.
      */
-    value = g_hash_table_lookup(config_hash, PCMK_OPT_STONITH_WATCHDOG_TIMEOUT);
+    value = g_hash_table_lookup(config_hash, PCMK_OPT_FENCING_WATCHDOG_TIMEOUT);
     controld_verify_stonith_watchdog_timeout(value);
 
     value = g_hash_table_lookup(config_hash, PCMK_OPT_NO_QUORUM_POLICY);

daemons/controld/controld_execd_state.c
@@ -507,7 +507,7 @@ crmd_remote_proxy_cb(lrmd_t *lrmd, void *userdata, xmlNode *msg)
             if (proxy != NULL) {
                 cib_t *cib_conn = controld_globals.cib_conn;
 
-                /* Look up PCMK_OPT_STONITH_WATCHDOG_TIMEOUT and send to the
+                /* Look up PCMK_OPT_FENCING_WATCHDOG_TIMEOUT and send to the
                  * remote peer for validation
                  */
                 int rc = cib_conn->cmds->query(cib_conn, PCMK_XE_CRM_CONFIG,

daemons/fenced/fenced_commands.c
@@ -1376,7 +1376,7 @@ fenced_device_register(const xmlNode *dev, bool from_cib)
 
         if (stonith_watchdog_timeout_ms <= 0) {
             crm_err("Ignoring watchdog fence device without "
-                    PCMK_OPT_STONITH_WATCHDOG_TIMEOUT " set");
+                    PCMK_OPT_FENCING_WATCHDOG_TIMEOUT " set");
             rc = ENODEV;
             goto done;
         }

daemons/fenced/fenced_remote.c
@@ -1525,14 +1525,14 @@ valid_fencing_timeout(int specified_timeout, bool action_specific,
     if (timeout > specified_timeout) {
         if (action_specific) {
             crm_warn("pcmk_%s_timeout %ds for %s is too short (must be >= "
-                     PCMK_OPT_STONITH_WATCHDOG_TIMEOUT " %ds), using %ds "
+                     PCMK_OPT_FENCING_WATCHDOG_TIMEOUT " %ds), using %ds "
                      "instead",
                      op->action, specified_timeout, device? device : "watchdog",
                      timeout, timeout);
 
         } else {
             crm_warn("Fencing timeout %ds is too short (must be >= "
-                     PCMK_OPT_STONITH_WATCHDOG_TIMEOUT " %ds), using %ds "
+                     PCMK_OPT_FENCING_WATCHDOG_TIMEOUT " %ds), using %ds "
                      "instead",
                      specified_timeout, timeout, timeout);
         }
@@ -2006,7 +2006,7 @@ request_peer_fencing(remote_fencing_op_t *op, peer_device_info_t *peer)
                  explicitly chosen for self-fencing. Local scheduler execution
                  in sbd might detect the node as unclean and lead to timely
                  self-fencing. Otherwise the selection of
-                 PCMK_OPT_STONITH_WATCHDOG_TIMEOUT at least is questionable.
+                 PCMK_OPT_FENCING_WATCHDOG_TIMEOUT at least is questionable.
              */
 
             /* coming here we're not waiting for watchdog timeout -

lib/common/options.c
@@ -259,7 +259,7 @@ static const pcmk__cluster_option_t cluster_options[] = {
         N_("This is set automatically by the cluster according to whether SBD "
             "is detected to be in use. User-configured values are ignored. "
             "The value `true` is meaningful if diskless SBD is used and "
-            "`stonith-watchdog-timeout` is nonzero. In that case, if fencing "
+            "`fencing-watchdog-timeout` is nonzero. In that case, if fencing "
             "is required, watchdog-based self-fencing will be performed via "
             "SBD without requiring a fencing resource explicitly configured."),
     },
@@ -274,7 +274,8 @@ static const pcmk__cluster_option_t cluster_options[] = {
          * calculate, and use 0 as the single default for when the option either
          * is unset or fails to validate.
          */
-        PCMK_OPT_STONITH_WATCHDOG_TIMEOUT, NULL, PCMK_VALUE_TIMEOUT, NULL,
+        PCMK_OPT_FENCING_WATCHDOG_TIMEOUT, PCMK_OPT_STONITH_WATCHDOG_TIMEOUT,
+            PCMK_VALUE_TIMEOUT, NULL,
         "0", NULL,
         pcmk__opt_controld,
         N_("How long before nodes can be assumed to be safely down when "

lib/common/watchdog.c
@@ -286,17 +286,17 @@ pcmk__valid_stonith_watchdog_timeout(const char *value)
 
         // At this point, 0 <= sbd_timeout <= st_timeout
         crm_debug("Using calculated value %lld for "
-                  PCMK_OPT_STONITH_WATCHDOG_TIMEOUT " (%s)",
+                  PCMK_OPT_FENCING_WATCHDOG_TIMEOUT " (%s)",
                   st_timeout, value);
     }
 
     if (st_timeout == 0) {
         crm_debug("Watchdog may be enabled but "
-                  PCMK_OPT_STONITH_WATCHDOG_TIMEOUT " is disabled (%s)",
+                  PCMK_OPT_FENCING_WATCHDOG_TIMEOUT " is disabled (%s)",
                   value? value : "default");
 
     } else if (pcmk__locate_sbd() == 0) {
-        crm_emerg("Shutting down: " PCMK_OPT_STONITH_WATCHDOG_TIMEOUT
+        crm_emerg("Shutting down: " PCMK_OPT_FENCING_WATCHDOG_TIMEOUT
                   " configured (%s) but SBD not active",
                   pcmk__s(value, "auto"));
         crm_exit(CRM_EX_FATAL);
@@ -306,16 +306,16 @@ pcmk__valid_stonith_watchdog_timeout(const char *value)
         long sbd_timeout = pcmk__get_sbd_watchdog_timeout();
 
         if (st_timeout < sbd_timeout) {
-            /* Passed-in value for PCMK_OPT_STONITH_WATCHDOG_TIMEOUT was
+            /* Passed-in value for PCMK_OPT_FENCING_WATCHDOG_TIMEOUT was
              * parsable, positive, and less than the SBD_WATCHDOG_TIMEOUT
              */
-            crm_emerg("Shutting down: " PCMK_OPT_STONITH_WATCHDOG_TIMEOUT
+            crm_emerg("Shutting down: " PCMK_OPT_FENCING_WATCHDOG_TIMEOUT
                       " (%s) too short (must be >%ldms)",
                       value, sbd_timeout);
             crm_exit(CRM_EX_FATAL);
             return false;
         }
-        crm_info("Watchdog configured with " PCMK_OPT_STONITH_WATCHDOG_TIMEOUT
+        crm_info("Watchdog configured with " PCMK_OPT_FENCING_WATCHDOG_TIMEOUT
                  " %s and SBD timeout %ldms",
                  value, sbd_timeout);
     }

commit 8ab88e1d70edd15836f927fac073d81499052e31

Fix: fencer: Make the deprecated "stonith-watchdog-timeout" option work again
This fixes a regression introduced by d7cc9b45a7, which has not made it
into made it into any release. Even though "stonith-watchdog-timeout"
option has become a deprecated alias for "fencing-watchdog-timeout", we
need to make sure it still works as a fallback until it's dropped in a
future release.

daemons/fenced/fenced_cib.c
@@ -146,6 +146,15 @@ fencing_topology_init(void)
                                "[@" PCMK_XA_NAME "='"   \
                                     PCMK_OPT_FENCING_WATCHDOG_TIMEOUT "']"
 
+/* @COMPAT The "stonith-watchdog-timeout" option has been a deprecated alias
+ * for "fencing-watchdog-timeout" since 3.0.2. Make sure it still works as a
+ * fallback until it's dropped in a future release.
+ */
+#define XPATH_STONITH_WATCHDOG_TIMEOUT "//" PCMK_XE_NVPAIR      \
+                                       "[@" PCMK_XA_NAME \
+                                            "='stonith-watchdog-timeout']"
+
+
 static void
 update_fencing_watchdog_timeout_ms(xmlNode *cib)
 {
@@ -160,6 +169,17 @@ update_fencing_watchdog_timeout_ms(xmlNode *cib)
     stonith_watchdog_xml = pcmk__xpath_find_one(cib->doc,
                                                 XPATH_WATCHDOG_TIMEOUT,
                                                 PCMK__LOG_NEVER);
+
+    /* @COMPAT The "stonith-watchdog-timeout" option has been a deprecated alias
+     * for "fencing-watchdog-timeout" since 3.0.2. Make sure it still works as a
+     * fallback until it's dropped in a future release.
+     */
+    if (stonith_watchdog_xml == NULL) {
+        stonith_watchdog_xml = pcmk__xpath_find_one(cib->doc,
+                                                    XPATH_STONITH_WATCHDOG_TIMEOUT,
+                                                    PCMK__LOG_NEVER);
+    }
+
     if (stonith_watchdog_xml == NULL) {
         return;
     }

lib/lrmd/lrmd_client.c
@@ -979,7 +979,7 @@ lrmd__validate_remote_settings(lrmd_t *lrmd, GHashTable *hash)
 
     pcmk__xe_set(data, PCMK__XA_LRMD_ORIGIN, __func__);
 
-    value = g_hash_table_lookup(hash, PCMK_OPT_FENCING_WATCHDOG_TIMEOUT);
+    value = pcmk__cluster_option(hash, PCMK_OPT_FENCING_WATCHDOG_TIMEOUT);
     if ((value) &&
         (stonith__watchdog_fencing_enabled_for_node(native->remote_nodename))) {
        pcmk__xe_set(data, PCMK__XA_LRMD_WATCHDOG, value);