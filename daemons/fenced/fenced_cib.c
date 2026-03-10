gao-yan/pacemaker

commit 215ad4f38216a423f755fa1a7762f0510d343073

Refactor: libcrmcommon: New pcmk__parse_ms()
To replace crm_get_msec().

There are a lot of parsing improvements that would be welcome (see
T841), but we'll have to be careful about making them. We're parsing
user inputs with this function. So changing how we respond to invalid
values, values that contain some garbage at the end, etc., affects user-
facing behavior, even though the function is internal.

This function basically preserves existing behavior for now, including
that it doesn't yet allow negative values. One key difference is that we
no longer use PCMK__PARSE_INT_DEFAULT inside it in any way. The output
argument (dest) remains unchanged if there is any error besides ERANGE.

Also improve unit tests for overflow, and add a test for negative input
that doesn't overflow.

(The watchdog parts got more complicated due to 11ab54a6, compared to
when I originally wrote this commit.)

Ref T841

Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

daemons/fenced/fenced_cib.c
@@ -149,34 +149,37 @@ fencing_topology_init(void)
 static void
 update_stonith_watchdog_timeout_ms(xmlNode *cib)
 {
-    long long timeout_ms = 0;
     xmlNode *stonith_watchdog_xml = NULL;
     const char *value = NULL;
+    int rc = pcmk_rc_ok;
 
     // @TODO An XPath search can't handle multiple instances or rules
     stonith_watchdog_xml = pcmk__xpath_find_one(cib->doc,
                                                 XPATH_WATCHDOG_TIMEOUT,
                                                 LOG_NEVER);
-    if (stonith_watchdog_xml) {
-        value = pcmk__xe_get(stonith_watchdog_xml, PCMK_XA_VALUE);
-    }
-    if (value != NULL) {
-        /* @COMPAT So far it has been documented that a negative value is
-         * valid. Parse it as an integer first to avoid the warning from
-         * crm_get_msec().
-         */
-        int rc = pcmk__scan_ll(value, &timeout_ms, PCMK__PARSE_INT_DEFAULT);
-
-        if (rc != pcmk_rc_ok || timeout_ms >= 0) {
-            timeout_ms = crm_get_msec(value);
-        }
+    if (stonith_watchdog_xml == NULL) {
+        return;
     }
 
-    if (timeout_ms < 0) {
-        timeout_ms = pcmk__auto_stonith_watchdog_timeout();
+    value = pcmk__xe_get(stonith_watchdog_xml, PCMK_XA_VALUE);
+    if (value == NULL) {
+        return;
     }
 
-    stonith_watchdog_timeout_ms = timeout_ms;
+    /* @COMPAT So far it has been documented that a negative value is valid.
+     * Parse it as an integer first to avoid the warning from pcmk__parse_ms().
+     * Skip pcmk__parse_ms() if we successfully parsed a negative value.
+     */
+    rc = pcmk__scan_ll(value, &stonith_watchdog_timeout_ms,
+                       PCMK__PARSE_INT_DEFAULT);
+
+    if ((rc != pcmk_rc_ok) || (stonith_watchdog_timeout_ms >= 0)) {
+        rc = pcmk__parse_ms(value, &stonith_watchdog_timeout_ms);
+    }
+
+    if ((rc != pcmk_rc_ok) || (stonith_watchdog_timeout_ms < 0)) {
+        stonith_watchdog_timeout_ms = pcmk__auto_stonith_watchdog_timeout();
+    }
 }
 
 /*!

daemons/attrd/attrd_attributes.c
@@ -60,14 +60,14 @@ attrd_create_attribute(xmlNode *xml)
     }
 
     if (dampen_s != NULL) {
-        dampen = crm_get_msec(dampen_s);
-    }
+        if ((pcmk__parse_ms(dampen_s, &dampen) != pcmk_rc_ok) || (dampen < 0)) {
+            crm_warn("Ignoring invalid delay %s for attribute %s", dampen_s,
+                     a->id);
 
-    if (dampen > 0) {
-        a->timeout_ms = (int) QB_MIN(dampen, INT_MAX);
-        a->timer = attrd_add_timer(a->id, a->timeout_ms, a);
-    } else if (dampen < 0) {
-        crm_warn("Ignoring invalid delay %s for attribute %s", dampen_s, a->id);
+        } else if (dampen > 0) {
+            a->timeout_ms = (int) QB_MIN(dampen, INT_MAX);
+            a->timer = attrd_add_timer(a->id, a->timeout_ms, a);
+        }
     }
 
     crm_trace("Created attribute %s with %s write delay and %s CIB user",
@@ -91,8 +91,7 @@ attrd_update_dampening(attribute_t *a, xmlNode *xml, const char *attr)
         return EINVAL;
     }
 
-    dampen = crm_get_msec(dvalue);
-    if (dampen < 0) {
+    if ((pcmk__parse_ms(dvalue, &dampen) != pcmk_rc_ok) || (dampen < 0)) {
         crm_warn("Could not update %s: invalid delay value %dms (%s)",
                  attr, dampen, dvalue);
         return EINVAL;

daemons/controld/controld_execd.c
@@ -1636,9 +1636,11 @@ construct_op(const lrm_state_t *lrm_state, const xmlNode *rsc_op,
 
         op_timeout = g_hash_table_lookup(params, "pcmk_monitor_timeout");
         if (op_timeout != NULL) {
-            long long timeout_ms = crm_get_msec(op_timeout);
+            long long timeout_ms = 0;
 
-            if (timeout_ms >= 0) {
+            if ((pcmk__parse_ms(op_timeout, &timeout_ms) == pcmk_rc_ok)
+                && (timeout_ms >= 0)) {
+
                 op->timeout = (int) QB_MIN(timeout_ms, INT_MAX);
             }
         }

daemons/fenced/fenced_commands.c
@@ -291,9 +291,11 @@ get_action_timeout(const fenced_device_t *device, const char *action,
         free(timeout_param);
 
         if (value) {
-            long long timeout_ms = crm_get_msec(value);
+            long long timeout_ms = 0;
 
-            if (timeout_ms >= 0) {
+            if ((pcmk__parse_ms(value, &timeout_ms) == pcmk_rc_ok)
+                && (timeout_ms >= 0)) {
+
                 int timeout_sec = 0;
 
                 timeout_ms = QB_MIN(timeout_ms, UINT_MAX);

include/crm/common/strings_internal.h
@@ -50,6 +50,7 @@ int pcmk__scan_ll(const char *text, long long *result, long long default_value);
 int pcmk__scan_min_int(const char *text, int *result, int minimum);
 int pcmk__scan_port(const char *text, int *port);
 int pcmk__parse_ll_range(const char *text, long long *start, long long *end);
+int pcmk__parse_ms(const char *input, long long *result);
 
 GHashTable *pcmk__strkey_table(GDestroyNotify key_destroy_func,
                                GDestroyNotify value_destroy_func);

lib/common/alerts.c
@@ -167,9 +167,11 @@ unpack_alert_options(xmlNode *xml, pcmk__alert_t *entry, guint *max_timeout)
 
     value = g_hash_table_lookup(config_hash, PCMK_META_TIMEOUT);
     if (value != NULL) {
-        long long timeout_ms = crm_get_msec(value);
+        long long timeout_ms = 0;
 
-        if (timeout_ms <= 0) {
+        if ((pcmk__parse_ms(value, &timeout_ms) != pcmk_rc_ok)
+            || (timeout_ms <= 0)) {
+
             entry->timeout = PCMK__ALERT_DEFAULT_TIMEOUT_MS;
 
             if (timeout_ms == 0) {

lib/common/fuzzers/strings_fuzzer.c
@@ -23,6 +23,7 @@ LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     char *ns = NULL;
     guint res = 0U;
+    long long msec = 0LL;
 
     if (size < 10) {
         return -1; // Do not add input to testing corpus
@@ -33,7 +34,7 @@ LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 
     pcmk__numeric_strcasecmp(ns, ns);
     pcmk_parse_interval_spec(ns, &res);
-    crm_get_msec(ns);
+    pcmk__parse_ms(ns, &msec);
 
     free(ns);
     return 0;

lib/common/strings.c
@@ -440,13 +440,12 @@ crm_get_msec(const char *input)
  * \param[in]  input      Pacemaker time interval specification (a bare number
  *                        of seconds; a number with a unit, optionally with
  *                        whitespace before and/or after the number; or an ISO
- *                        8601 duration)
+ *                        8601 duration) (can be \c NULL)
  * \param[out] result_ms  Where to store milliseconds equivalent of \p input on
  *                        success (limited to the range of an unsigned integer),
  *                        or 0 if \p input is \c NULL or invalid
  *
- * \return Standard Pacemaker return code (specifically, \c pcmk_rc_ok if
- *         \p input is valid or \c NULL, and \c EINVAL otherwise)
+ * \return Standard Pacemaker return code
  */
 int
 pcmk_parse_interval_spec(const char *input, guint *result_ms)
@@ -469,14 +468,18 @@ pcmk_parse_interval_spec(const char *input, guint *result_ms)
         }
 
     } else {
-        msec = crm_get_msec(input);
+        rc = pcmk__parse_ms(input, &msec);
     }
 
     if (msec < 0) {
         crm_warn("Using 0 instead of invalid interval specification '%s'",
                  input);
         msec = 0;
-        rc = EINVAL;
+
+        if (rc == pcmk_rc_ok) {
+            // Preserve any error from pcmk__parse_ms()
+            rc = EINVAL;
+        }
     }
 
 done:
@@ -880,6 +883,115 @@ pcmk__parse_ll_range(const char *text, long long *start, long long *end)
 
 done:
     g_strfreev(split);
+    return rc;
+}
+
+/*!
+ * \internal
+ * \brief Parse a time and units string into a milliseconds value
+ *
+ * \param[in]  input   String with a nonnegative number and optional unit
+ *                     (optionally with whitespace before and/or after the
+ *                     number). If absent, the unit defaults to seconds.
+ * \param[out] result  Where to store result in milliseconds (unchanged on error
+ *                     except \c ERANGE)
+ *
+ * \return Standard Pacemaker return code
+ */
+int
+pcmk__parse_ms(const char *input, long long *result)
+{
+    long long local_result = 0;
+    char *units = NULL; // Do not free; will point to part of input
+    long long multiplier = 1000;
+    long long divisor = 1;
+    int rc = pcmk_rc_ok;
+
+    CRM_CHECK(input != NULL, return EINVAL);
+
+    // Skip initial whitespace
+    for (; isspace(*input); input++);
+
+    rc = scan_ll(input, &local_result, 0, &units);
+
+    if (local_result < 0) {
+        crm_warn("'%s' is not a valid time duration: Negative", input);
+        return pcmk_rc_bad_input;
+    }
+
+    if (rc == ERANGE) {
+        crm_warn("'%s' will be clipped to %lld", input, local_result);
+
+        /* Continue through rest of body before returning ERANGE
+         *
+         * @COMPAT Improve handling of overflow. Units won't necessarily be
+         * respected right now, for one thing.
+         */
+
+    } else if (rc != pcmk_rc_ok) {
+        crm_warn("'%s' is not a valid time duration: %s", input,
+                 pcmk_rc_str(rc));
+        return rc;
+    }
+
+    /* If the number is a decimal, scan_ll() reads only the integer part. Skip
+     * any remaining digits or decimal characters.
+     *
+     * @COMPAT Well-formed and malformed decimals are both accepted inputs. For
+     * example, "3.14 ms" and "3.1.4 ms" are treated the same as "3ms" and
+     * parsed successfully. At a compatibility break, decide if this is still
+     * desired.
+     */
+    for (; isdigit(*units) || (*units == '.'); units++);
+
+    // Skip any additional whitespace after the number
+    for (; isspace(*units); units++);
+
+    /* @COMPAT Use exact comparisons. Currently, we match too liberally, and the
+     * second strncasecmp() in each case is redundant.
+     */
+    if ((*units == '\0')
+        || (strncasecmp(units, "s", 1) == 0)
+        || (strncasecmp(units, "sec", 3) == 0)) {
+        multiplier = 1000;
+        divisor = 1;
+
+    } else if ((strncasecmp(units, "ms", 2) == 0)
+               || (strncasecmp(units, "msec", 4) == 0)) {
+        multiplier = 1;
+        divisor = 1;
+
+    } else if ((strncasecmp(units, "us", 2) == 0)
+               || (strncasecmp(units, "usec", 4) == 0)) {
+        multiplier = 1;
+        divisor = 1000;
+
+    } else if ((strncasecmp(units, "m", 1) == 0)
+               || (strncasecmp(units, "min", 3) == 0)) {
+        multiplier = 60 * 1000;
+        divisor = 1;
+
+    } else if ((strncasecmp(units, "h", 1) == 0)
+               || (strncasecmp(units, "hr", 2) == 0)) {
+        multiplier = 60 * 60 * 1000;
+        divisor = 1;
+
+    } else {
+        // Invalid units
+        return pcmk_rc_bad_input;
+    }
+
+    if (result == NULL) {
+        return rc;
+    }
+
+    // Apply units, capping at LLONG_MAX
+    if (local_result > (LLONG_MAX / multiplier)) {
+        *result = LLONG_MAX;
+    } else {
+        *result = (local_result * multiplier) / divisor;
+    }
+
     return rc;
 }
 

commit 35d0b3da51052dc150256b8a159980722814979b

Fix: fencer: Disable watchdog-based self-fencing if "fencing-watchdog-timeout" is unset
This fixes a regression introduced by 215ad4f382, which has not made it
into any release.

daemons/fenced/fenced_cib.c
@@ -153,6 +153,9 @@ update_fencing_watchdog_timeout_ms(xmlNode *cib)
     const char *value = NULL;
     int rc = pcmk_rc_ok;
 
+    // Reset the value in case the cluster option is unset
+    fencing_watchdog_timeout_ms = 0;
+
     // @TODO An XPath search can't handle multiple instances or rules
     stonith_watchdog_xml = pcmk__xpath_find_one(cib->doc,
                                                 XPATH_WATCHDOG_TIMEOUT,